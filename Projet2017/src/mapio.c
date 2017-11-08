dc6106d497b5845d6ffe3708c7a1652228d88fe3
BordeauxFac <33452649+BordeauxFac@users.noreply.github.com>
2017-11-08 10:07:52 +0100

    Fonction save complète et fonctionnelle

--- Projet2017/src/mapio.c
+++ Projet2017/src/mapio.c
@@ -8,6 +8,7 @@
 #include "error.h"
 
 #ifdef PADAWAN
+static char * solidite[]={"air", "semi-solid","solid","liquid"};
 
 void map_new (unsigned width, unsigned height)
 {
@@ -22,104 +23,129 @@ void map_new (unsigned width, unsigned height)
   }
 
   map_object_begin (8);
-  
-    // Texture pour le sol
-    map_object_add ("images/ground.png", 1, MAP_OBJECT_SOLID);
-    // Mur
-    map_object_add ("images/wall.png", 1, MAP_OBJECT_SOLID);
-    // Gazon
-    map_object_add ("images/grass.png", 1, MAP_OBJECT_SEMI_SOLID);
-    // Marbre
-    map_object_add ("images/marble.png", 1, MAP_OBJECT_SOLID | MAP_OBJECT_DESTRUCTIBLE);
-    // Herbe
-    map_object_add ("images/herb.png", 1, MAP_OBJECT_AIR);
-    // Petit plancher flottant
-    map_object_add ("images/floor.png", 1, MAP_OBJECT_SEMI_SOLID);
-    //fleur
-    map_object_add("images/flower.png",1,MAP_OBJECT_AIR);
-    //pièces
-    map_object_add("images/coin.png",20,MAP_OBJECT_AIR|MAP_OBJECT_COLLECTIBLE);
-  
-    map_object_end ();
+
+  // Texture pour le sol
+  map_object_add ("images/ground.png", 1, MAP_OBJECT_SOLID);
+  // Mur
+  map_object_add ("images/wall.png", 1, MAP_OBJECT_SOLID);
+  // Gazon
+  map_object_add ("images/grass.png", 1, MAP_OBJECT_SEMI_SOLID);
+  // Marbre
+  map_object_add ("images/marble.png", 1, MAP_OBJECT_SOLID | MAP_OBJECT_DESTRUCTIBLE);
+  // Herbe
+  map_object_add ("images/herb.png", 1, MAP_OBJECT_AIR);
+  // Petit plancher flottant
+  map_object_add ("images/floor.png", 1, MAP_OBJECT_SEMI_SOLID);
+  // Pièces
+  map_object_add ("images/coin.png", 20, MAP_OBJECT_COLLECTIBLE);
+  // Fleurs
+  map_object_add ("images/flower.png", 1, MAP_OBJECT_AIR);
+
+  map_object_end ();
 }
 
 void map_save (char *filename)
 {
   // On commence par créer un fichier de sauvegarde
-    int sauvegarde=open(filename,O_CREAT | O_TRUNC | O_WRONLY , 0666);
-    if (sauvegarde == -1){
-	    fprintf(stderr,"Erreur lors de la création du fichier de sauvegarde ");
-	    return NULL;
-    }
+  int sauvegarde=open(filename,O_CREAT | O_TRUNC | O_WRONLY , 0666);
+  if (sauvegarde == -1){
+	fprintf(stderr,"Erreur lors de la création du fichier de sauvegarde ");
+	return NULL;
+  }
   
   //On redirige ensuite la sortie standard directement dans le fichier sauvegarde ( lignes de 	codes moins compliqué qu'avec des writes etc... ) , il ne faut pas oublier de sauvegarder la sortie standard !
-    int sauvegarde_stdout=dup(1);
-    int redirection=dup2(sauvegarde,1);
-    if (redirection ==-1){
-	      fprintf(stderr,"Erreur lors de la redirection de stdout ");
-    }
+  int sauvegarde_stdout=dup(1);
+  int redirection=dup2(sauvegarde,1);
+  if (redirection ==-1){
+	fprintf(stderr,"Erreur lors de la redirection de stdout ");
+  }
   
 
   //Il faut ensuite récupérer les dimensions de la carte , on choisit unsigned int car on a pas besoin des nombres négatifs du int 
-    unsigned int largeur=map_width(); // largeur de la map
-    unsigned int hauteur=map_height(); //hauteur de la map
-    unsigned int nb_objet=map_objects();
+  unsigned int largeur=map_width(); // largeur de la map
+  unsigned int hauteur=map_height(); //hauteur de la map
+  unsigned int nb_objet=map_objects();
+  int wr;
   
   // La même chose en 3 lignes
-    printf("%u\t",largeur);
-    printf("%u\t",hauteur);
-    printf("%u\t",nb_objet);
+  printf("%u\t",largeur);
+  printf("%u\t",hauteur);
+  printf("%u\n",nb_objet); // on saute une ligne
+
+
+  //Une version avec des writes , 
+  /*wr=write(sauvegarde,&largeur,sizeof(unsigned int));
+  if(wr<0){
+	fprintf(stderr,"Erreur lors de l'écriture de la largeur");
+  }
+  wr=write(sauvegarde,&hauteur,sizeof(unsigned int));
+  if(wr<0){
+	fprintf(stderr,"Erreur lors de l'écriture de la hauteur");
+  }
+   wr=write(sauvegarde,&nb_objet,sizeof(unsigned int));
+  if(wr<0){
+	fprintf(stderr,"Erreur lors de l'écriture du nombre d objet");
+  }
+  // On revient ensuite à la ligne 
+  printf("\n");*/
+  
+
+  /*On écrit ensuite la liste des objets disponibles pour la carte avec leurs caractéristiques :
+ nom , frames , si ils sont solides , destructibles , collectables ...  Pour gérer la solidité des objets nous avons rajouté un tableaux en début de fichier
+	il pourrait être judicieux et beucoup plus simple de créer également des tableaux pour les autres paramètres afin de raccourcir le code ci-dessous */
   char * info;
   // On sépare chaque écriture par une tabulation , sinon c'est le bazar !
-      for(int i=0;i<nb_objet;i++){
-	      printf("%s\t",map_get_name(i)); // on récupére le nom de l'objet
-	      printf("%d\t",map_get_frames(i)); // on récupére le nombre de frame
-	      printf("%s\t",solidite[map_get_solidity(i)]);
-	      if (map_is_destructible(i)==1){
-		      info="destructible";
-		      printf("%s\t",info);
-      	}
-      	else{
-	      	info="not-destructible";
-		      printf("%s\t",info);
-      	}
-	      if (map_is_collectible(i)==1){
-		      info="collectible";
-		      printf("%s\t",info);
-	      }
-	      else{
-		      info="not-collectible";
-		      printf("%s\t",info);
-	      }
-	      if (map_is_generator(i)==1){
-	      	info="generator";
-		      printf("%s\t",info);
-	      }
-        else{
-		      info="not-generator";
-	      	printf("%s\n",info); // je met un \n pour revenir à la ligne
-      	}
-      }
+  for(int i=0;i<nb_objet;i++){
+	printf("%s\t",map_get_name(i)); // on récupére le nom de l'objet
+	printf("%d\t",map_get_frames(i)); // on récupére le nombre de frame
+	printf("%s\t",solidite[map_get_solidity(i)]);
+	if (map_is_destructible(i)==1){
+		info="destructible";
+		printf("%s\t",info);
+	}
+	else{
+		info="not-destructible";
+		printf("%s\t",info);
+	}
+	if (map_is_collectible(i)==1){
+		info="collectible";
+		printf("%s\t",info);
+	}
+	else{
+		info="not-collectible";
+		printf("%s\t",info);
+	}
+	if (map_is_generator(i)==1){
+		info="generator";
+		printf("%s\t",info);
+	}
+	else{
+		info="not-generator";
+		printf("%s\n",info); // je met un \n pour revenir à la ligne
+	}
+  }
   /* Il ne reste plus qu'à sauvegarder la position des objets sur la carte ainsi que le type d'objet ( fleurs pièces ....)  .
 					On effectue un parcours en largeur*/
 
   int type;
   for(int y=0;y<hauteur;y++){
-	  for(int x=0;x<largeur;x++){
-		  if(type=map_get(x,y) != MAP_OBJECT_NONE){
-			  printf("%d\t",x);// sauvegarde x
-			  printf("%d\t",y);// sauvegarde y
-			  printf("%d",type);
-			  printf("\n");
-		  }
-	  }
+	for(int x=0;x<largeur;x++){
+		if(type=map_get(x,y) != MAP_OBJECT_NONE){
+			printf("%d\t",x);// sauvegarde x
+			printf("%d\t",y);// sauvegarde y
+			printf("%d",type);
+			printf("\n");
+		}
+	}
   }
-  fflush(stdout);
   close(sauvegarde);
   dup2(sauvegarde_stdout,1); // on rétablie la sortie standard
   printf("La carte a été sauvegardée \n ");
-}
+  
+  
 
+  
+}
 
 void map_load (char *filename)
 {

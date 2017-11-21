d9a522aa1a22420d1a9836b9edeb72fa2baa1bcb
BordeauxFac <33452649+BordeauxFac@users.noreply.github.com>
2017-11-21 17:21:05 +0100

    Add files via upload
    Fonction setWidth Fonctionne ! faut faire un switch case

--- Projet2017/util/maputil.c
+++ Projet2017/util/maputil.c
@@ -52,41 +52,63 @@ unsigned int getNbObject(int file){
 
 							/*Fonctions setteurs des données de la carte */
 void setWidth(int file,unsigned int width){
-	unsigned int oLargeur=getWidth(file);// on sauvegarde l'ancienne largeur pour plus tard
-	unsigned int nLargeur=width;
+	unsigned int old_width=getWidth(file);// on sauvegarde l'ancienne largeur pour plus tard
+	unsigned int new_width=width;
 	lseek(file,0,SEEK_SET);//On se met au début du fichier pour remplacer l'ancienne valeur de la largeur
-	write(file,&nLargeur,sizeof(unsigned int));//on remplace l'ancienne valeur
+	write(file,&new_width,sizeof(unsigned int));//on remplace l'ancienne valeur
 
 	/*On doit ensutie regarder si la nouvelle largeur est plus petite que l'ancienne , si 
 			c'est le cas on doit supprimer les objets de la carte qui 
 				n'apparaitront pas à l'écran
 							/* main  qui doit tester si l'argument est uen fonction valide et doit l'éxécuter */
 
-	if (nLargeur< oLargeur) {
+	if (new_width< old_width) {
 
-	/* On va créer un fichier temporaire pour recopier le fichier actuel dans le fichier temporaire avec les  nouvelles dimensions et en ne recopiant que les objets aux bonnes coordonnées !*/
+	  /* On va créer un fichier temporaire pour recopier le fichier actuel dans le fichier temporaire avec les  nouvelles dimensions et en ne recopiant que les objets aux bonnes coordonnées !*/
 
-	/*On commence par réecrire les dimensions de la carte */
-	int temp=("temp.txt",O_CREAT | O_TRUNC | O_RDONLY| O_WRONLY , 0666);
-	write(temp,&nLargeur,sizeof(unsigned int));
-	unsigned int hauteur=getHeight(file);
-	write(temp,&hauteur,sizeof(unsigned int));
-	unsigned int val=getNbObject(file);// le curseur de file est à la quatrieme valeur
-	write(temp,&val,sizeof(unsigned int));
-
-	/*On recopie le type d'objet qui sont dans les nouvelles dimensions de la carte */
-	for(int i=0;i<oLargeur;i++){
-		if(i<=nLargeur){//on recopie si on est plus petit que la nouvelle largeur
-			for(int j=0;j<hauteur;i++){
-				read(file,&val,sizeof(unsigned int));
-				write(temp,&val,sizeof(unsigned int));
-			}
-		}
+	  /*On commence par réecrire les dimensions de la carte */
+	  int temp=open("../maps/save2.map",O_CREAT | O_TRUNC | O_RDONLY| O_WRONLY , 0666);
+	  write(temp,&new_width,sizeof(unsigned int));
+	  unsigned int height=getHeight(file);
+	  write(temp,&height,sizeof(unsigned int));
+	  unsigned int nbObject=getNbObject(file);// le curseur de file est à la quatrieme valeur
+	  write(temp,&nbObject,sizeof(unsigned int));
+	  unsigned int val;
+	  /*On recopie le type d'objet qui sont dans les nouvelles dimensions de la carte */
+	  for(int i=0;i<old_width;i++){
+	    //on recopie si on est plus petit que la nouvelle largeur
+	    for(int j=0;j<height;j++){
+	      read(file,&val,sizeof(unsigned int));
+	      if(i<new_width){
+		write(temp,&val,sizeof(unsigned int));
+	      }
+	    } 
+	  }
+	   
+	  for(int i=0;i<nbObject;i++){
+	    unsigned int filenameSize=0;
+	    read(file,&filenameSize,sizeof(unsigned int));
+	    write(temp,&filenameSize,sizeof(unsigned int));
+	    for(int j=0;j<filenameSize;j++){
+	      char car=0;
+	      read(file,&car,sizeof(char));
+	      write(temp,&car,sizeof(char));
+	    }
+	    write(temp,'\0',sizeof(char));
+	    unsigned int frames , solidity , destructible,collectible,generator;
+	    read(file,&frames,sizeof(unsigned int));
+	    read(file,&solidity,sizeof(unsigned int));
+	    read(file,&destructible,sizeof(unsigned int));
+	    read(file,&collectible,sizeof(unsigned int));
+	    read(file,&generator,sizeof(unsigned int));
+	    write(temp,&frames,sizeof(unsigned int));
+	     write(temp,&solidity,sizeof(unsigned int));
+	      write(temp,&destructible,sizeof(unsigned int));
+	       write(temp,&collectible,sizeof(unsigned int));
+	        write(temp,&generator,sizeof(unsigned int));
+	  }
+	  	close(temp);
 	}
-	}
-
-	/*Il faut ensuite recopié la fin du fichier file c'est à dire les différents objets 
-			de la carte avec leurs caractèristiques*/
 
 }
 void main(int argc,char ** argv){
@@ -94,9 +116,12 @@ void main(int argc,char ** argv){
 	if (file==-1){
 		bigError("Erreur lors de l'ouverture du fichier ");
 	}
-	//Cas du getHeight
-	printf("la hauteur est %u\n",getHeight(file));
-	//Cas du getWidth
+	char * c=argv[2];
+	switch(c){
+	case '"--getWidth"':
+	  printf("la largeur est %u\n",getWidth(file));break;
+	}
+	/*//Cas du getWidth
 	printf("la largeur est %u\n",getWidth(file));
 	//Cas du getNbObejct
 	printf("le nombre d'objet est %u\n",getNbObject(file));
@@ -104,8 +129,11 @@ void main(int argc,char ** argv){
 	//Cas du getInfo
 	printf("la largeur est %u\n",getWidth(file));
 	printf("la hauteur est %u\n",getHeight(file));
-	printf("le nombre d'objet est %u\n",getNbObject(file));
-
+	printf("le nombre d'objet est %u\n",getNbObject(file));*/
+	if(strcmp(c,"--setWidth")==0){
+	    unsigned int a=atoi(argv[3]);
+	    setWidth(file,a);
+	  }
+	  
+	    
 }
-	
-	

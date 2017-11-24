1e49dfedc9e736c9f982ece3bf98fc4b75b6d5c5
Abdoul Diallo <adiallo004@emi.u-bordeaux1.fr>
2017-11-24 10:45:57 +0100

    just check

--- Projet2017/util/function.h
+++ Projet2017/util/function.h
@@ -1,7 +1,22 @@
 #ifndef FUNCTION_H
 #define FUNCTION_H
 
+//structure definissant un objet
+typedef struct
+{
+   char * filename;
+   unsigned int filenameSize;
+
+   unsigned int frames;
+   unsigned int solidity;
+   unsigned int destructible;
+   unsigned int collectible;
+   unsigned int generator;
+}Object;
+
+
 void bigError(char * message);
+bool comparer(Object obj1,Object obj2);
 unsigned int getWidth(int file);
 unsigned int getHeight(int file);
 unsigned int getNbObject(int file);
@@ -10,4 +25,6 @@ void copyWrite(int src,int dst,int count,void * buf);
 void setWidth(int file,unsigned int width);
 void setHeight(int file,unsigned int height);
 
+void pruneObject(int file,Object objectToremove);
+void addObject(int file,Object * ObjectToadd);
 #endif
